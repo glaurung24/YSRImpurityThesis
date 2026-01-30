ae9d112299272a83e8d4be21ece81e97f73482cc
Andreas Theiler <andreas.theiler@gmx.at>
2026-01-30 19:19:29 +0100

    Changed to include block index, results possible false

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include "TBTK/Solver/Diagonalizer.h"
 #include "TBTK/TBTK.h"
 #include "TBTK/Visualization/MatPlotLib/Plotter.h"
+#include "TBTK/Solver/BlockDiagonalizer.h"
+#include "TBTK/BlockStructureDescriptor.h"
 #include <complex>
 using namespace std;
 using namespace TBTK;
@@ -35,25 +37,25 @@ const bool SELF_CONSISTENCY = true;
 const bool USE_GPU = false;
 const bool USE_MULTI_GPU = false;
 
-double getParity(PropertyExtractor::Diagonalizer& pe, unsigned sizeX, unsigned sizeY){
-	double n_up = 0;
-	double n_down = 0;
-	double h_up = 0;
-	double h_down = 0;
-	for(unsigned x = 0; x < sizeX; ++x){
-		for(unsigned y = 0; y < sizeX; ++y){
+// double getParity(PropertyExtractor::Diagonalizer& pe, unsigned sizeX, unsigned sizeY){
+// 	double n_up = 0;
+// 	double n_down = 0;
+// 	double h_up = 0;
+// 	double h_down = 0;
+// 	for(unsigned x = 0; x < sizeX; ++x){
+// 		for(unsigned y = 0; y < sizeX; ++y){
 			
-			n_up += real(pe.calculateExpectationValue({x, y, 0, 0}, {x, y, 0, 0}));
-			n_down += real(pe.calculateExpectationValue({x, y, 1, 0}, {x, y, 1, 0}));
-			h_up += real(pe.calculateExpectationValue({x, y, 0, 1}, {x, y, 0, 1}));
-			h_down += real(pe.calculateExpectationValue({x, y, 1, 1}, {x, y, 1, 1}));
-		}
-	}
-	std::cout << "Nup: " << n_up << ", Ndown: " << n_down << std::endl;
-	std::cout << "Hup: " << h_up << ", Hdown: " << h_down << std::endl;
-	std::cout << "Total: " << h_up + h_down + n_up + n_down << std::endl;
-	return n_up + n_down;
-}
+// 			n_up += real(pe.calculateExpectationValue({0, 0, x, y}, {0, 0, x, y}));
+// 			n_down += real(pe.calculateExpectationValue({1, 0, x, y}, {1, 0, x, y}));
+// 			h_up += real(pe.calculateExpectationValue({0, 1, x, y}, {0, 1,x, y}));
+// 			h_down += real(pe.calculateExpectationValue({1, 1, x, y}, {1, 1, x, y}));
+// 		}
+// 	}
+// 	std::cout << "Nup: " << n_up << ", Ndown: " << n_down << std::endl;
+// 	std::cout << "Hup: " << h_up << ", Hdown: " << h_down << std::endl;
+// 	std::cout << "Total: " << h_up + h_down + n_up + n_down << std::endl;
+// 	return n_up + n_down;
+// }
 
 bool selfConsistencyStep(Solver::Diagonalizer solver){
 	PropertyExtractor::Diagonalizer property_extractor(solver);
@@ -70,9 +72,9 @@ bool selfConsistencyStep(Solver::Diagonalizer solver){
 			for(unsigned spin = 0; spin < 2; ++spin){
 				// Gap equation
 				Delta[{x, y}] += 0.5*V_sc *(-1.+2*spin) * property_extractor.calculateExpectationValue(
-					{x,y, (spin+1)%2, 1 },
-					{x,y, spin, 0 }
-				);;
+					{(spin+1)%2, 1, x,y},
+					{spin, 0, x,y}
+				);
 			}
 		}
 	}
@@ -103,10 +105,10 @@ class DeltaCallback : public HoppingAmplitude::AmplitudeCallback{
 		const Index &from
 	) const{
 		//Obtain indices
-		unsigned int x = from[0];
-		unsigned int y = from[1];
-		unsigned int spin = from[2];
-		unsigned int particleHole = from[3];
+		unsigned int x = from[2];
+		unsigned int y = from[3];
+		unsigned int spin = from[0];
+		unsigned int particleHole = from[1];
 
 		if(spin == 0 && particleHole == 0)
 			return conj(Delta[{x, y}]);
@@ -143,8 +145,8 @@ public:
         const Index &to,
         const Index &from
     ) const{
-        Subindex spin = from[2];
-        Subindex particleHole = from[3];
+        Subindex spin = from[0];
+        Subindex particleHole = from[1];
         return J*(1. - 2*spin)*(1. - 2*particleHole);
     }
     //Set the value for J.
@@ -154,6 +156,25 @@ public:
 private:
     complex<double> J;
 };
+
+
+// This function is supposed to divide the Hamiltonian into two blocks with the first block being particle spin up and hole spin down.
+// The second block is the other combination 
+
+int getBlockIndex(
+    const Index &index) {
+    Subindex spin = index[0];
+    Subindex particleHole = index[1];
+
+    if(spin == particleHole){
+        return 0;
+    }
+    else{
+        return 1;
+    }
+}
+
+
 int main(){
     //Initialize TBTK.
     Initialize();
@@ -161,7 +182,7 @@ int main(){
     const unsigned int SIZE_X = 11;
     const unsigned int SIZE_Y = 11;
     const double t = -1;
-    const double mu = -2;
+    const double mu = 0;
     const double Delta = 0.5;
     //Create a callback that returns the Zeeman term and that will be used
     //as input to the Model.
@@ -172,40 +193,44 @@ int main(){
         for(unsigned int y = 0; y < SIZE_Y; y++){
             for(unsigned int spin = 0; spin < 2; spin++){
                 for(unsigned int ph = 0; ph < 2; ph++){
+                    int block = getBlockIndex({spin, ph});
                     model << HoppingAmplitude(
                         -mu*(1. - 2*ph),
-                        {x, y, spin, ph},
-                        {x, y, spin, ph}
+                        {block, spin, ph, x, y},
+                        {block, spin, ph, x, y}
                     );
                     if(x+1 < SIZE_X){
                         model << HoppingAmplitude(
                             t*(1. - 2*ph),
-                            {x+1, y, spin, ph},
-                            {x, y, spin, ph}
+                            {block, spin, ph, x+1, y},
+                            {block, spin, ph, x, y}
                         ) + HC;
                     }
                     if(y+1 < SIZE_Y){
                         model << HoppingAmplitude(
                             t*(1. - 2*ph),
-                            {x, y+1, spin, ph},
-                            {x, y, spin, ph}
+                            {block, spin, ph, x, y+1},
+                            {block, spin, ph, x, y}
                         ) + HC;
                     }
                 }
+                int block_to = getBlockIndex({spin, 0});
+                int block_from = getBlockIndex({(spin+1)%2, 1});
                 model << HoppingAmplitude(
                     Delta*(1. - 2*spin),
-                    {x, y, spin, 0},
-                    {x, y, (spin+1)%2, 1}
+                    {block_to, spin, 0, x, y},
+                    {block_from, (spin+1)%2, 1, x, y}
                 ) + HC;
             }
         }
     }
     for(unsigned int spin = 0; spin < 2; spin++){
         for(unsigned int ph = 0; ph < 2; ph++){
+            int block = getBlockIndex({spin, ph});
             model << HoppingAmplitude(
                 jCallback,
-                {SIZE_X/2, SIZE_Y/2, spin, ph},
-                {SIZE_X/2, SIZE_Y/2, spin, ph}
+                {block, spin, ph, SIZE_X/2, SIZE_Y/2},
+                {block, spin, ph, SIZE_X/2, SIZE_Y/2}
             );
         }
     }
@@ -220,6 +245,15 @@ int main(){
     });
     //Iterate over 100 values for J.
     Range j(0, 5, NUM_ITERATIONS);
+
+
+    // BlockStructureDescriptor blockStructureDescriptor = BlockStructureDescriptor(
+	// 			model.getHoppingAmplitudeSet());
+    // cout << blockStructureDescriptor.getNumBlocks() << endl;
+    // exit(0);
+    // Solver::BlockDiagonalizer blockSolver;
+    // blockSolver.setModel(model);
+
     for(unsigned int n = 0; n < NUM_ITERATIONS; n++){
         //Update the callback with the current value of J.
         jCallback.setJ(j[n]);
@@ -235,7 +269,7 @@ int main(){
             10000000
         );
 		std::cout << "J: " << j[n] << std::endl;
-		getParity(propertyExtractor, SIZE_X, SIZE_Y) ;
+		// getParity(propertyExtractor, SIZE_X, SIZE_Y) ;
         //Calculate the eigenvalues.
         Property::EigenValues eigenValues
             = propertyExtractor.getEigenValues();
@@ -249,8 +283,8 @@ int main(){
             RESOLUTION
         );
         Property::LDOS ldos = propertyExtractor.calculateLDOS({
-            {SIZE_X/2, SIZE_Y/2, IDX_SUM_ALL, IDX_SUM_ALL},
-            {SIZE_X/4, SIZE_Y/4, IDX_SUM_ALL, IDX_SUM_ALL}
+            {IDX_SUM_ALL, IDX_SUM_ALL, IDX_SUM_ALL, SIZE_X/2, SIZE_Y/2},
+            {IDX_SUM_ALL, IDX_SUM_ALL, IDX_SUM_ALL, SIZE_X/4, SIZE_Y/4}
         });
         //Smooth the LDOS.
         const double SMOOTHING_SIGMA = 0.1;
@@ -264,10 +298,11 @@ int main(){
         for(unsigned int e = 0; e < ldos.getResolution(); e++){
             totalLdos[{n, e}] = ldos(
                 {
-                    SIZE_X/2,
-                    SIZE_Y/2,
                     IDX_SUM_ALL,
-                    IDX_SUM_ALL
+                    IDX_SUM_ALL,
+                    IDX_SUM_ALL,
+                    SIZE_X/2,
+                    SIZE_Y/2
                 },
                 e
             );
